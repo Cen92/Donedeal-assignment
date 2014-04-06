69c1c5fda616b1960fd7793c9cbec17c4d71d519
Cen Breathnach <cen.breathnach1@gmail.com>
2014-04-06 11:51:59 +0100

    Pagination working

--- DoneDeal Assignment/BeerTableViewController.m
+++ DoneDeal Assignment/BeerTableViewController.m
@@ -8,25 +8,24 @@
 
 #import "BeerTableViewController.h"
 #import <AFNetworking/AFNetworking.h>
+
 @interface BeerTableViewController ()
 @property (strong, nonatomic) NSMutableArray *beerArray;
-@property int *pagesLoaded;
-@property int *numberOfPages;
+@property (strong, nonatomic) NSNumber *pages;
+@property int pagesLoaded;
 @property BOOL isLoading;
 @end
 
 @implementation BeerTableViewController
 
-
-
-
 - (void)viewDidLoad
 {
+    
     [super viewDidLoad];
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
-	[self makeJSONRequest:(int)1];
-    // Do any additional setup after loading the view, typically from a nib.
+    _pagesLoaded = 1;
+	[self makeJSONRequest:(int)_pagesLoaded]; //load page one of results
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,47 +37,47 @@
 #pragma mark - AFNetworking Reqs
 
 -(void)makeJSONRequest:(int)pageNumber{
-    NSMutableString *searchURL = [NSMutableString stringWithString:@"https://api.brewerydb.com/v2/beers/?key=3e6731ff19dc2830a7f15f90c50c7fb9&format=json&styleId=5&p="];
+    NSMutableString *searchURL = [NSMutableString stringWithString:@"https://api.brewerydb.com/v2/beers/?key=3e6731ff19dc2830a7f15f90c50c7fb9&format=json&styleId=5&p="]; //limit of 400 reqs a day with free account
     NSMutableString *page = [NSMutableString stringWithFormat:@"%d", pageNumber];
-    [searchURL appendString:page];
+    [searchURL appendString:page]; //generating URL for paginated results
     NSURL *url = [NSURL URLWithString:searchURL]; //create url object
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     operation.responseSerializer = [AFJSONResponseSerializer serializer];
     [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
 
-        if(!_beerArray){
-            _beerArray = [[responseObject objectForKey:@"data"]mutableCopy];
-            
+        if(!_beerArray){ //if array is empty
+            _beerArray = [[responseObject objectForKey:@"data"]mutableCopy]; // set array to be a copy of data
+            _pages = [responseObject objectForKey:@"numberOfPages"]; //get total number of pages from the response
         }
         else{
-            [_beerArray addObjectsFromArray:[[responseObject objectForKey:@"data"]mutableCopy]];
+            [_beerArray addObjectsFromArray:[[responseObject objectForKey:@"data"]mutableCopy]]; //add new objects to end of array
         }
+        [self.tableView reloadData]; //reload table view with new data
         _isLoading = NO;
-        [self.tableView reloadData];
-        
+    
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         _isLoading = NO;
         NSLog(@"Request Failed: %@, %@", error, error.userInfo);
-       // [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-
-        
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving List"
+                                                            message:[error localizedDescription]
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"Ok"
+                                                  otherButtonTitles:nil];
+        [alertView show];
     }];
-    
     [operation start];
-
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;// Return the number of sections.
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    // Return the number of rows in the section.
     return [_beerArray count];
 }
 
@@ -88,18 +87,15 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
     NSDictionary *beerDictionary = [_beerArray objectAtIndex:indexPath.row];
     cell.textLabel.text = [beerDictionary objectForKey:@"name"];
-
-    
-    if(indexPath.row == [_beerArray count]-5){
-        if(!_isLoading){
+    cell.textLabel.adjustsFontSizeToFitWidth=YES; //reduces font size for beers with longer names
+    if(indexPath.row == [_beerArray count]-5){ //if we are 5 cells from the end of the list, load next page
+        if(!_isLoading){ //prevents spamming of requests
+            if(_pagesLoaded < [_pages integerValue]){ //if there are pages to load
             _isLoading = YES;
-        _pagesLoaded++;
-        [self makeJSONRequest:(int)_pagesLoaded];
+            _pagesLoaded++;
+            [self makeJSONRequest:(int)_pagesLoaded];
+            }
         }
-        else{
-            NSLog(@"Loading...");
-        }
-        _isLoading = NO;
     }
     return cell;
 }
